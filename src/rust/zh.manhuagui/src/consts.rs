682287e6156bd4db0fdac74a500d701c231bd7db
StoneMoe <lakechan96@gmail.com>
2022-07-20 13:53:35 +0800

    fix: list fetching

---
+++ src/rust/zh.manhuagui/src/consts.rs
@@ -0,0 +1,64 @@
+// Server Endpoints
+pub const BASE_URL: &str = "https://tw.manhuagui.com";
+const IMAGE_SERVERS: [&str; 2] = ["https://i.hamreus.com", "https://cf.hamreus.com"];
+
+// 排序
+pub const FILTER_SORT: [&str; 4] = ["view", "", "update", "rate"];
+// 地区
+pub const FILTER_AREA: [&str; 7] = ["", "japan", "hongkong", "other", "europe", "china", "korea"];
+// 剧情
+pub const FILTER_GENRE: [&str; 39] = [
+    "",
+    "rexue",
+    "maoxian",
+    "mohuan",
+    "shengui",
+    "gaoxiao",
+    "mengxi",
+    "aiqing",
+    "kehuan",
+    "mofa",
+    "gedou",
+    "wuxia",
+    "jizhan",
+    "zhanzheng",
+    "jingji",
+    "tiyu",
+    "xiaoyuan",
+    "shenghuo",
+    "lizhi",
+    "lishi",
+    "weiniang",
+    "zhainan",
+    "funv",
+    "danmei",
+    "baihe",
+    "hougong",
+    "zhiyu",
+    "meishi",
+    "tuili",
+    "xuanyi",
+    "kongbu",
+    "sige",
+    "zhichang",
+    "zhentan",
+    "shehui",
+    "yinyue",
+    "wudao",
+    "zazhi",
+    "heidao",
+];
+// 受众
+pub const FILTER_READER: [&str; 6] = ["", "shaonv", "shaonian", "qingnian", "ertong", "tongyong"];
+// 年份
+pub const FILTER_PUBLISH_DATE: [&str; 16] = [
+    "", "2020", "2019", "2018", "2017", "2016", "2015", "2014", "2013", "2012", "2011", "2010",
+    "200x", "199x", "198x", "197x",
+];
+// 首字母
+pub const FILTER_FIRST_LETTER: [&str; 28] = [
+    "", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
+    "s", "t", "u", "v", "w", "x", "y", "z", "0-9",
+];
+// 进度
+pub const FILTER_STATUS: [&str; 3] = ["", "lianzai", "wanjie"];
--- src/rust/zh.manhuagui/src/helper.rs
+++ src/rust/zh.manhuagui/src/helper.rs
@@ -1,66 +1,77 @@
-use aidoku::{std::net::HttpMethod, std::net::Request, std::String, std::Vec};
+use aidoku::prelude::*;
+use aidoku::{
+	error::Result, std::String, std::Vec, Filter,
+	FilterType,
+};
+use alloc::string::ToString;
 
-use base64ct::{Base64, Encoding};
+use crate::consts::*;
+use crate::{manga_list, utils};
 
-pub fn encode_uri(string: &String) -> String {
-	let mut result: Vec<u8> = Vec::with_capacity(string.len() * 3);
-	let hex = "0123456789abcdef".as_bytes();
-	let bytes = string.as_bytes();
+pub fn fetch_manga_list(filters: Vec<Filter>, page: i32) -> Result<manga_list::Resp> {
+	let mut query: String = String::new();
+	let mut filters_list: Vec<&str> = Vec::new();
+	let mut sort_opt: String = String::new();
 
-	for byte in bytes {
-		let curr = *byte;
-		if (b'a'..=b'z').contains(&curr)
-			|| (b'A'..=b'Z').contains(&curr)
-			|| (b'0'..=b'9').contains(&curr)
-			|| (curr == b';'
-				|| curr == b',' || curr == b'/'
-				|| curr == b'?' || curr == b':'
-				|| curr == b'@' || curr == b'&'
-				|| curr == b'=' || curr == b'+'
-				|| curr == b'$')
-			|| (curr == b'-'
-				|| curr == b'_' || curr == b'.'
-				|| curr == b'!' || curr == b'~'
-				|| curr == b'*' || curr == b'\''
-				|| curr == b'(' || curr == b')')
-			|| (curr == b'#')
-		{
-			result.push(curr);
-		} else {
-			result.push(b'%');
-			result.push(hex[curr as usize >> 4]);
-			result.push(hex[curr as usize & 15]);
+	// prepare filter
+	for filter in filters {
+		match filter.kind {
+			FilterType::Title => {
+				query = filter.value.as_string()?.read();
+			}
+			FilterType::Select => {
+				let index = filter.value.as_int()? as usize;
+				let filter_value = match filter.name.as_str() {
+					"地区" => FILTER_AREA[index],
+					"剧情" => FILTER_GENRE[index],
+					"受众" => FILTER_READER[index],
+					"年份" => FILTER_PUBLISH_DATE[index],
+					"首字母" => FILTER_FIRST_LETTER[index],
+					"进度" => FILTER_STATUS[index],
+					_ => continue,
+				};
+
+				if filter_value != "" {
+					filters_list.push(filter_value);
+				}
+			}
+			FilterType::Sort => {
+				let value = match filter.value.as_object() {
+					Ok(value) => value,
+					Err(_) => continue,
+				};
+				let sort_index = value.get("index").as_int()? as usize;
+				sort_opt = FILTER_SORT[sort_index].to_string();
+			}
+			_ => continue,
 		}
 	}
 
-	String::from_utf8(result).unwrap_or_default()
-}
-
-pub fn i32_to_string(mut integer: i32) -> String {
-	if integer == 0 {
-		return String::from("0");
-	}
-	let mut string = String::with_capacity(11);
-	let pos = if integer < 0 {
-		string.insert(0, '-');
-		1
+	return if !query.is_empty() {
+		let url = format!(
+			"{}/s/{}_p{}.html",
+			BASE_URL,
+			&utils::encode_uri(&query),
+			page
+		);
+		println!("[zh-manhuagui]search fetching: {}", url);
+		manga_list::parse(utils::get(&url).html(), manga_list::ParseMode::Search)
 	} else {
-		0
+		// Example: https://www.manhuagui.com/list/japan_maoxian_qingnian_2020_b/update_p1.html
+		//                                        /$params
+		// /$sort_p$page.html
+		let url = format!(
+			"{}/list/{}/{}_p{}.html",
+			BASE_URL,
+			filters_list.join("_"),
+			if sort_opt.is_empty() {
+				"index"
+			} else {
+				&sort_opt
+			},
+			page
+		);
+		println!("[zh-manhuagui]filter fetching: {}", url);
+		manga_list::parse(utils::get(&url).html(), manga_list::ParseMode::Filtered)
 	};
-	while integer != 0 {
-		let mut digit = integer % 10;
-		if pos == 1 {
-			digit *= -1;
-		}
-		string.insert(pos, char::from_u32((digit as u32) + ('0' as u32)).unwrap());
-		integer /= 10;
-	}
-	string
-}
-
-pub fn get(url: &str) -> Request {
-	Request::new(url, HttpMethod::Get)
-    .header("Referer", "https://tw.manhuagui.com/")
-    .header("User-Agent",
-    "Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.93 Mobile Safari/537.36 Aidoku/1.0")
 }
